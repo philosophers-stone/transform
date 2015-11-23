4871e7ae5309fd9411f2ff147e89598068d20ae1
Booker C. Bense <bbense@gmail.com>
2015-11-23 10:51:40 -0800

    Still need to get Struct working

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# transform
-An Elixir Protocol for transforming arbitrary Elixir data structures.
+# Transform
+
+**A Protocol Implementation for Transforming arbitrary Elixir data Structures**
+
+## Documentation 
+
+The `Transform` protocol will convert any Elixir data structure
+using a given transform into a new data structure. 
+
+The `transform/3` function takes the data structure and 
+a map of transformation functions and a depth level. It will
+then does a depth-first recursion through the structure, 
+applying the tranformation functions for all 
+data types found in the data structure. 
+
+The transform map has data types as keys and 
+anonymous functions as values. The anonymous
+functions have the data item and recursion depth
+as inputs and can return anything. 
+
+	transformer = %{ Atom => fn(atom, depth) -> atom end }
+
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed as:
+
+  1. Add transform to your list of dependencies in `mix.exs`:
+
+        def deps do
+          [{:transform, "~> 0.0.1"}]
+        end
+
+  2. Ensure transform is started before your application:
+
+        def application do
+          [applications: [:transform]]
+        end
---
+++ test/transform_test.exs
@@ -0,0 +1,123 @@
+
+defmodule TransformTest do
+  use ExUnit.Case
+
+  test "identity is the default atom" do 
+  	assert Transform.transform(:foo,%{}) == :foo 
+  end 
+
+  test "convert to string" do 
+    trans = %{ Atom => fn(x, _d) -> Atom.to_string(x) end}
+  	assert Transform.transform(:foo,trans) == "foo"
+  end 
+
+  test "identity is the default string" do 
+    assert Transform.transform("foo",%{}) == "foo"
+  end 
+
+  test "convert string to atom" do 
+    trans = %{ BitString => fn(x, _d) -> String.to_atom(x) end}
+    assert Transform.transform("foo",trans) == :foo
+  end 
+
+  test "identity is the default Integer" do 
+    assert Transform.transform(1,%{}) == 1
+  end 
+
+  test "convert integer to string" do 
+    trans = %{ Integer => fn(x, _d) -> Integer.to_string(x) end}
+    assert Transform.transform(1,trans) == "1"
+  end 
+
+  test "identity is the default Float" do 
+    assert Transform.transform(1.0, %{}) == 1.0
+  end 
+
+  test "convert Float to string" do 
+    trans = %{ Float => fn(x, _d) -> Float.to_string(x) end}
+    assert Transform.transform(5.0, trans) == "5.00000000000000000000e+00"
+  end 
+
+
+  test "identity is the default list" do 
+    assert Transform.transform([1,2,3],%{}) == [1,2,3]
+  end 
+
+  test "convert list elements to atom" do 
+    foo = ["a", "b", "c"]
+    trans = %{ BitString => fn(x, _d) -> String.to_atom(x) end }
+    assert Transform.transform(foo,trans) == [:a, :b, :c]
+  end 
+
+  test "convert list to tuple" do 
+    foo = [:a, :b, :c]
+    trans = %{ List => fn(x, _d) -> List.to_tuple(x) end }
+    assert Transform.transform(foo,trans) == {:a, :b, :c}
+  end 
+
+
+  test "nested list to nested tuple" do 
+    foo = [[:a], [:b], [:c]]
+    trans = %{ List => fn(x, _d) -> List.to_tuple(x) end }
+    assert Transform.transform(foo,trans) == {{:a}, {:b}, {:c}}
+  end 
+
+  test "identity is the default tuple" do 
+    assert Transform.transform({1, 2, 3}, %{}) == {1, 2, 3}
+  end 
+
+  test "convert tuple elements to atom" do 
+    foo = {"a", "b", "c"}
+    trans = %{ BitString => fn(x, _d) -> String.to_atom(x) end }
+    assert Transform.transform(foo, trans) == {:a, :b, :c}
+  end 
+
+  test "convert tuple to list" do 
+    foo = {:a, :b, :c}
+    trans = %{ Tuple => fn(x, _d) -> Tuple.to_list(x) end }
+    assert Transform.transform(foo, trans) == [:a, :b, :c]
+  end 
+
+  test "nested tuple to list" do 
+    foo = {{:a}, {:b}, {:c}}
+    trans = %{ Tuple => fn(x, _d) -> Tuple.to_list(x) end }
+    assert Transform.transform(foo, trans) == [[:a], [:b], [:c]]
+  end 
+
+  test "identity is the default map" do 
+    foo = %{"a" => 1, "b" => 2}
+    assert Transform.transform(foo, %{}) == foo
+  end 
+
+  test "convert map to keyword list" do
+    foo = %{"a" => 1, "b" => 2}
+    bar = [a: 1, b: 2]
+    to_keyword = fn(m, _d) -> 
+      for {k, v} <- m , into: [], do: {String.to_atom(k), v} 
+    end
+    trans = %{Map => to_keyword}
+    assert Transform.transform(foo, trans) == bar
+  end 
+
+  test "identity is the default struct" do 
+    assert Transform.transform(1..5, %{}) == 1..5
+  end 
+
+  test "identity is the default quote do output" do 
+    foo = quote do: Enum.map(1..3, fn(x) -> x*x end) |> Enum.sum
+    assert Transform.transform(foo, %{}) == foo 
+  end 
+
+  test "transform quote do output" do
+    foo = quote do: Enum.map(1..3, fn(x) -> x*x end)
+    bar = quote do: Enum.map(1..3, fn(y) -> y*y end)
+    replace_x = fn(a, _d ) -> 
+      case a do 
+        :x -> :y 
+        atom -> atom
+      end 
+    end 
+    trans = %{ Atom => replace_x }
+    assert Transform.transform(foo, trans) == bar
+  end 
+end
