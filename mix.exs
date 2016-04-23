20dbd6d627a9d48aaa1507146656a9b485a2c523
Booker C. Bense <bbense@gmail.com>
2016-04-22 19:22:38 -0700

    validate example working

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PhStTransform.Mixfile do
 
   def project do
     [app: :phst_transform,
-     version: "0.9.0",
+     version: "1.0.0",
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
