468432812aeb6317dc47eb867889dff46d577970
Booker C. Bense <bbense@gmail.com>
2017-01-05 20:58:06 -0800

    Update version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PhStTransform.Mixfile do
 
   def project do
     [app: :phst_transform,
-     version: "1.0.0",
+     version: "1.0.1",
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
